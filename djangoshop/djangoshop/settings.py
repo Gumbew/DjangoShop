012de71cbcbb17f550b925ac4b13c08b0f16f39b
Valentyn Khoroshchak <smartgtsua@gmail.com>
2020-02-21 01:00:30 +0200

    Revert "Removed explicit data from settings.py"
    This reverts commit 34ce2b6f805b4fc877c89d20d766559ddb62eddf.

--- djangoshop/djangoshop/settings.py
+++ djangoshop/djangoshop/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 
@@ -22,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = '%yw*9k6x+riel(0^$k@5hjih^pwz%j-+o_xu*5vtl3kal-nq08'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -137,5 +136,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = 'khoroshchak.valentin@gmail.com'
+EMAIL_HOST_PASSWORD = 'Valik1999Valik'
